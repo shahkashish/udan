1f7ca2bcfb775fa4bd15e900aa9d46ab976779b9
Kashish Shah <shahkashish.in@gmail.com>
2024-11-06 01:19:58 +0530

    updated get_bikes_type

--- api/get_bike_types.js
+++ api/get_bike_types.js
@@ -3,22 +3,15 @@ const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient("https://asbphrnlbbgpphwryywe.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzYnBocm5sYmJncHBod3J5eXdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4MjEyNDgsImV4cCI6MjA0NjM5NzI0OH0.wf9BNvUQ7_yHyKbLUK04AeyX4UN46eXOlxK4ieYylqE");
 
 module.exports = async (req, res) => {
-    try {
-        let { data, error } = await supabase
-            .from("stockStatus")
-            .select("*");
+    
+    let { data, error } = await supabase
+    .from("stockStatus")
+    .select("bikeName")
+    console.log(data)   
 
-        console.log("Data:", data);
-        console.log("Error:", error);
-
-        if (error) {
-            res.status(500).json({ error: error.message });
-        } else {
-            res.json(data);  // Directly return the data to see what's in the response
-        }
-    } catch (err) {
-        console.error("Unexpected error:", err);
-        res.status(500).json({ error: err.message });
+    if (error) {
+        res.status(500).json({ error: error.message });
+    } else {
+        res.json(data.map(row => row.type));
     }
 };
-
