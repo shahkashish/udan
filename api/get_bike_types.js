bc3cd55ecca7251d9409def8d4c5c432d4e3cea7
Kashish Shah <shahkashish.in@gmail.com>
2024-11-06 00:35:50 +0530

    updated get_bikes_type

--- api/get_bike_types.js
+++ api/get_bike_types.js
@@ -3,9 +3,11 @@ const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient("https://asbphrnlbbgpphwryywe.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzYnBocm5sYmJncHBod3J5eXdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4MjEyNDgsImV4cCI6MjA0NjM5NzI0OH0.wf9BNvUQ7_yHyKbLUK04AeyX4UN46eXOlxK4ieYylqE");
 
 module.exports = async (req, res) => {
-    const { data, error } = await supabase
-        .from('stockStatus')
-        .select('bikeName')
+    
+    let { data: stockStatus, error } = await supabase
+    .from('stockStatus')
+    .select('*')
+    console.log(data)   
 
     if (error) {
         res.status(500).json({ error: error.message });
