6f2d27c95f5e8d361a5c7f32a84d706617f2ad9b
Kashish Shah <shahkashish.in@gmail.com>
2024-11-06 01:22:20 +0530

    updated get_bikes_type

--- api/get_bike_types.js
+++ api/get_bike_types.js
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
     
     let { data, error } = await supabase
     .from("stockStatus")
-    .select("bikeName")
+    .select('bikeName')
     console.log(data)   
 
     if (error) {
