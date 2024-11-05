5c7de341987e662607365e2eaf2107706248b014
Kashish Shah <shahkashish.in@gmail.com>
2024-11-06 01:08:17 +0530

    updated get_bikes_type

--- api/get_bike_types.js
+++ api/get_bike_types.js
@@ -2,23 +2,29 @@ const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient("https://asbphrnlbbgpphwryywe.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzYnBocm5sYmJncHBod3J5eXdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4MjEyNDgsImV4cCI6MjA0NjM5NzI0OH0.wf9BNvUQ7_yHyKbLUK04AeyX4UN46eXOlxK4ieYylqE");
 
-module.exports = async (req, res) => {
-    try {
-        let { data, error } = await supabase
-            .from("stockStatus")
-            .select("*");
 
-        console.log("Data:", data);
-        console.log("Error:", error);
+
+
+
+async function getTables() {
+    try {
+        const { data, error } = await supabase
+            .rpc('get_tables'); // Call the RPC function to get tables
 
         if (error) {
-            res.status(500).json({ error: error.message });
-        } else {
-            res.json(data);  // Directly return the data to see what's in the response
+            console.error('Error fetching tables:', error);
+            return [];
         }
+
+        return data;
     } catch (err) {
-        console.error("Unexpected error:", err);
-        res.status(500).json({ error: err.message });
+        console.error('Unexpected error:', err);
+        return [];
     }
-};
+}
+
+// Execute the function and log the results
+getTables().then(tables => {
+    console.log('List of Tables:', tables);
+});
 
