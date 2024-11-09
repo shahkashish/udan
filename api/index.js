96c385829892a6462bb0e0b070d5c47537c66400
Kashish Shah <shahkashish.in@gmail.com>
2024-11-09 23:46:27 +0530

    added api endpoints and modified existing endpoints

--- api/index.js
+++ api/index.js
@@ -1,24 +1,28 @@
-const express = require('express');
-const { createClient } = require('@supabase/supabase-js');
+// const express = require('express');
+// const { createClient } = require('@supabase/supabase-js');
+// const path = require('path');
+// const app = express();
+
+// app.use(express.json({ limit: '10mb' }));
+// app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// // Serve static files from the public directory
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+// });
+
+// // Route to serve the Change Status page
+// app.get('/changeStatus', (req, res) => {
+//     res.sendFile(path.join(__dirname, '..', 'public', 'changeStatus.html'));
+// });
+
+// app.get('/uploadData', (req, res) => {
+//     res.sendFile(path.join(__dirname, '..', 'public', 'uploadData.html'));
+// });
+
+// // Export the app as a serverless function
+// module.exports = app;
 const path = require('path');
-const app = express();
-
-app.use(express.json({ limit: '10mb' }));
-app.use(express.static(path.join(__dirname, '..', 'public')));
-
-// Serve static files from the public directory
-app.get('/', (req, res) => {
+module.exports = (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-});
-
-// Route to serve the Change Status page
-app.get('/changeStatus', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'changeStatus.html'));
-});
-
-app.get('/uploadData', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'uploadData.html'));
-});
-
-// Export the app as a serverless function
-module.exports = app;
+};
