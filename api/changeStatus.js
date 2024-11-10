230514de9432f10fd33b42616d85d251aaa84426
Kashish Shah <shahkashish.in@gmail.com>
2024-11-10 13:33:39 +0530

    added routes for navigating to other webpages

--- api/changeStatus.js
+++ api/changeStatus.js
@@ -1,5 +1,23 @@
+const express = require('express');
 const path = require('path');
+const app = express();
 
-module.exports = (req, res) => {
+app.use(express.json({ limit: '10mb' }));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Serve static files from the public directory
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+// Route to serve the Change Status page
+app.get('/changeStatus', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'changeStatus.html'));
-};
+});
+
+app.get('/uploadExcel', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'uploadExcel.html'));
+});
+
+// Export the app as a serverless function
+module.exports = app;
--- api/index.js
+++ api/index.js
@@ -1,28 +1,27 @@
-// const express = require('express');
-// const { createClient } = require('@supabase/supabase-js');
-// const path = require('path');
-// const app = express();
-
-// app.use(express.json({ limit: '10mb' }));
-// app.use(express.static(path.join(__dirname, '..', 'public')));
-
-// // Serve static files from the public directory
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-// });
-
-// // Route to serve the Change Status page
-// app.get('/changeStatus', (req, res) => {
-//     res.sendFile(path.join(__dirname, '..', 'public', 'changeStatus.html'));
-// });
-
-// app.get('/uploadData', (req, res) => {
-//     res.sendFile(path.join(__dirname, '..', 'public', 'uploadData.html'));
-// });
-
-// // Export the app as a serverless function
-// module.exports = app;
+const express = require('express');
 const path = require('path');
-module.exports = (req, res) => {
+const app = express();
+
+app.use(express.json({ limit: '10mb' }));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Serve static files from the public directory
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-};
+});
+
+// Route to serve the Change Status page
+app.get('/changeStatus', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'changeStatus.html'));
+});
+
+app.get('/uploadExcel', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'uploadExcel.html'));
+});
+
+// Export the app as a serverless function
+module.exports = app;
+// const path = require('path');
+// module.exports = (req, res) => {
+//     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+// };
